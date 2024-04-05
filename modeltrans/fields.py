b17e2d4f412b95416a44672e7805a1a05d8b475e
Jacob Wegner <jacobwegner@gmail.com>
2024-04-05 15:25:27 -0500

    Add JSONField as a supported field

--- modeltrans/fields.py
+++ modeltrans/fields.py
@@ -12,7 +12,7 @@ from .utils import (
     get_language,
 )
 
-SUPPORTED_FIELDS = (fields.CharField, fields.TextField)
+SUPPORTED_FIELDS = (fields.CharField, fields.TextField, JSONField)
 
 DEFAULT_LANGUAGE = get_default_language()
 
